6e2e3a3d5808b1a3d6240796296a8b6dedc431b5
atevsl <3380033@ukr.net>
2023-02-09 23:12:50 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,21 @@ import Searchbar from '../Searchbar/Searchbar';
 
 import css from './App.module.css';
 const APIkey = '32042597-d449e2f3b6adbf69100237dc7';
+// const Status = {
+//   IDLE: 'idle',
+//   PENDING: 'pending',
+//   RESOLVED: 'resolved',
+//   REJECTED: 'rejected',
+// };
 
 export class App extends React.Component {
   state = {
     imgSearchName: '',
     imgsToDisplay: [],
     page: 1,
+    loading: false,
+    loadingFailure: false,
   };
-
   onSubmitHendler = imgSearchName => {
     this.setState({ imgSearchName });
   };
@@ -34,18 +41,23 @@ export class App extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.imgSearchName !== this.state.imgSearchName) {
-      this.setState({ page: 1 });
+      this.setState({ page: 1, loading: true, loadingFailure: false });
+
       this.onFetchHendler()
         .then(data => {
-          console.log(data);
-          console.log(data.hits);
-          this.setState({ imgsToDisplay: data.hits });
+          if (data.total === 0) {
+            this.setState({ loadingFailure: true });
+            return;
+          }
+          this.setState({ imgsToDisplay: data.hits, loading: false });
         })
         .catch(error => {
           console.log(error);
-        });
+        })
+        .finally(this.setState({ oading: false }));
     }
     if (prevState.page !== this.state.page) {
+      this.setState({ loading: true });
       this.onFetchHendler()
         .then(data => {
           this.setState(prevstate => ({
@@ -54,20 +66,29 @@ export class App extends React.Component {
         })
         .catch(error => {
           console.log(error);
-        });
+        })
+        .finally(this.setState({ loading: false }));
     }
   }
   render() {
     return (
       <div className={css.AppWrap}>
         <Searchbar onSubmitHendler={this.onSubmitHendler}></Searchbar>
-        <ImageGallery
-          imgsToDisplay={this.state.imgsToDisplay}
-          onModalShow={this.onModalShow}
-        ></ImageGallery>
+        {!this.state.loading && (
+          <ImageGallery
+            imgsToDisplay={this.state.imgsToDisplay}
+            onModalShow={this.onModalShow}
+          ></ImageGallery>
+        )}
 
-        {this.state.imgsToDisplay.length === 0 && <Loader></Loader>}
-        {this.state.imgsToDisplay.length !== 0 && (
+        {this.state.loading && <Loader></Loader>}
+        {this.state.loadingFailure && (
+          <p>
+            Oops! Sorry, we couldnt find {this.state.imgSearchName}, what you
+            are looking for. 😭
+          </p>
+        )}
+        {this.state.imgsToDisplay.length !== 0 && !this.state.loading && (
           <Button onLoadMoreHendler={this.onLoadMoreHendler}></Button>
         )}
       </div>
