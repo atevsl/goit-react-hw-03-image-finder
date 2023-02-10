ca00acbb0b6c09e1455719cd41de95c34cf74323
atevsl <3380033@ukr.net>
2023-02-10 12:42:55 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import css from './Modal.module.css';
-const Modal = props => {
-  return (
-    <div className={css.Overlay}>
-      <div className={css.Modal}>
-        <img src={props.img.src} alt={props.img.alt} />
+class Modal extends React.Component {
+  onClickHehdler = e => {
+    if (e.target === e.currentTarget) {
+      console.log('кликнули по бєкдропу-закрываем модалку');
+    }
+  };
+  onEscClose = e => {
+    console.log(e);
+    if (e.key === 'Escape') {
+      console.log('кликнули по button esc');
+    }
+  };
+
+  componentDidMount() {
+    const modal = document.querySelector('#modal');
+    modal.addEventListener('click', this.onClickHehdler);
+    window.addEventListener('keypress', this.onEscClose);
+  }
+  render() {
+    return (
+      <div className={css.Overlay} id="modal">
+        <div className={css.Modal}>
+          <img src={this.props.img.src} alt={this.props.img.alt} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Modal;
